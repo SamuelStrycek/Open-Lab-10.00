fe13dfa3db6ccc4a1c0db6348a8594a598e1484e
SamuelStrycek <91180378+SamuelStrycek@users.noreply.github.com>
2021-12-02 19:48:55 +0100

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,81 @@
-﻿using System;
+using System;
 
-namespace ConsoleApp1
+namespace Open_Lab__10._03
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Book Lotr = new Book();
+            Lotr.Title = "Lord of the Rings";
+            Lotr.Pages = 458;
+            Lotr.Category = "fantast";
+            Lotr.Autor = "J.R.R. Tolkien";
+            Lotr.ReleaseDate = 1954;
+            Lotr.WriteParameters();
+
+
         }
+
+        class Book
+        {
+            public string Title { get; set; }
+            public string Category { get; set; }
+            public string Autor { get; set; }
+
+
+            private int pages;
+            public int Pages
+            {
+                get
+                {
+                    return pages;
+                }
+                set
+                {
+                    if (value < 0)
+                    {
+                        pages = 1;
+                    }
+                    else
+                    {
+                        pages = value;
+                    }
+                }
+            }
+
+            private int releaseDate;
+            public int ReleaseDate
+            {
+                get
+                {
+                    return releaseDate;
+                }
+                set
+                {
+                    if (value < 1450 && value > 2021)
+                    {
+                        releaseDate = -1;
+                    }
+                    else
+                    {
+                        releaseDate = value;
+                    }
+                }
+            }
+
+            public void WriteParameters()
+            {
+                Console.WriteLine(Title);
+                Console.WriteLine(Pages);
+                Console.WriteLine(Category);
+                Console.WriteLine(Autor);
+                Console.WriteLine(ReleaseDate);
+
+
+            }
+        }
+
     }
 }
